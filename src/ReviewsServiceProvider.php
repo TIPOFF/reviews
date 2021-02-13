3d606f7a7fcef7698a7486bbc02d4c3c3e771c09
auxiliawebsitedesign <auxiliawebsitedesign@users.noreply.github.com>
2021-02-13 21:59:23 +0000

    Fix styling

--- src/ReviewsServiceProvider.php
+++ src/ReviewsServiceProvider.php
@@ -8,7 +8,6 @@ use Tipoff\Reviews\Models\Competitor;
 use Tipoff\Reviews\Models\Insight;
 use Tipoff\Reviews\Models\Review;
 use Tipoff\Reviews\Models\Snapshot;
-use Tipoff\Reviews\Models\Key;
 use Tipoff\Reviews\Policies\CompetitorPolicy;
 use Tipoff\Reviews\Policies\InsightPolicy;
 use Tipoff\Reviews\Policies\ReviewPolicy;

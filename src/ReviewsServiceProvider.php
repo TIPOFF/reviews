66a59e90bc6840e832bb8e67142929b36f826daf
auxiliawebsitedesign <preston@auxiliawebsitedesign.com>
2021-02-13 16:58:47 -0500

    Update composer.json and ReviewsServiceProvider.php

--- src/ReviewsServiceProvider.php
+++ src/ReviewsServiceProvider.php
@@ -4,43 +4,28 @@ declare(strict_types=1);
 
 namespace Tipoff\Reviews;
 
-use Illuminate\Support\Facades\Gate;
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Tipoff\Reviews\Models\Competitor;
 use Tipoff\Reviews\Models\Insight;
 use Tipoff\Reviews\Models\Review;
 use Tipoff\Reviews\Models\Snapshot;
+use Tipoff\Reviews\Models\Key;
 use Tipoff\Reviews\Policies\CompetitorPolicy;
 use Tipoff\Reviews\Policies\InsightPolicy;
 use Tipoff\Reviews\Policies\ReviewPolicy;
 use Tipoff\Reviews\Policies\SnapshotPolicy;
 
-class ReviewsServiceProvider extends PackageServiceProvider
+class ReviewsServiceProvider extends TipoffServiceProvider
 {
-    public function boot()
-    {
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-
-        parent::boot();
-    }
-
-    public function configurePackage(Package $package): void
+    public function configureTipoffPackage(TipoffPackage $package): void
     {
         $package
+            ->hasPolicies([
+                Competitor::class => CompetitorPolicy::class,
+                Insight::class => InsightPolicy::class,
+                Review::class => ReviewPolicy::class,
+                Snapshot::class => SnapshotPolicy::class,
+            ])
             ->name('reviews')
             ->hasConfigFile();
-    }
-
-    public function registeringPackage()
-    {
-        foreach ([
-            Competitor::class => CompetitorPolicy::class,
-            Insight::class => InsightPolicy::class,
-            Review::class => ReviewPolicy::class,
-            Snapshot::class => SnapshotPolicy::class,
-        ] as $key => $policy) {
-            Gate::policy($key, $policy);
-        }
     }
 }

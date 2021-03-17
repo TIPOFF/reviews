9ee94c9cfd7ac3b23bba120d4edcc69117def112
dj <djamesfar@gmail.com>
2021-03-17 12:11:57 -0700

    return all Review attributes

--- src/Http/Resources/ReviewResource.php
+++ src/Http/Resources/ReviewResource.php
@@ -14,6 +14,13 @@ class ReviewResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'title' => $this->title,
+            'text' => $this->text,
+            'name' => $this->name,
+            'initial' => $this->initial,
+            'photo' => $this->photo,
+            'date' => $this->date,
+        ];
     }
 }

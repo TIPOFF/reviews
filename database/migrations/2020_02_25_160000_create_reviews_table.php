e0d9f33bdea0c35b4ac43c4ca6d4c248fe509728
Drew Roberts <github@drewroberts.com>
2021-03-17 13:26:30 -0400

    Update migration for reviews #8

--- database/migrations/2020_02_25_160000_create_reviews_table.php
+++ database/migrations/2020_02_25_160000_create_reviews_table.php
@@ -12,6 +12,7 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(app('location'));
             $table->string('google_ref')->unique(); // Google Review ID reference
             $table->unsignedTinyInteger('rating');
             $table->text('comment')->nullable();
@@ -22,12 +23,13 @@ class CreateReviewsTable extends Migration
             $table->text('reply')->nullable();
             $table->dateTime('replied_at')->nullable(); // Really is the last time the reply was updated
             $table->boolean('is_displayable')->default(false); // Mark true if review should be displayed on website
-            $table->boolean('is_photo_displayable')->default(false); // Mark true if website should display the reviewer_photo as well or just use the yellow placeholder
+            $table->boolean('is_photo_displayable')->default(false); // Mark true if website should display the reviewer_photo as well or just use the reviewer image placeholder
             $table->string('reviewer_displayable')->nullable(); // Allows modifying reviewer name for display on website. If null, will just use actual reviewer name.
             $table->string('title_displayable')->nullable(); // Quick bold title excerpt from the comment. If null, will default to "5 out of 5 stars!" or some generic title based on 5 star rating
             $table->text('comment_displayable')->nullable(); // Allows modifying review comment for display on website (mostly for grammar, not content). If null, will just use actual review comment.
-
-            $table->foreignIdFor(app('location'));
+            
+            $table->foreignIdFor(app('user'), 'creator_id')->nullable();
+            $table->foreignIdFor(app('user'), 'updater_id')->nullable();
             $table->timestamps();
         });
     }

8c03cab84082d20b9423f06f464438bb384defbb
Drew Roberts <github@drewroberts.com>
2021-03-19 06:07:18 -0400

    Add google ref number as unique review id #9

--- src/Http/Resources/ReviewResource.php
+++ src/Http/Resources/ReviewResource.php
@@ -15,6 +15,7 @@ class ReviewResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'ref' => $this->google_ref,
             'title' => $this->title,
             'text' => $this->text,
             'name' => $this->name,

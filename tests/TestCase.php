004929394f3564a54120d48ab3c527961429e4ec
Drew Roberts <github@drewroberts.com>
2021-03-23 23:43:04 -0400

    Import service providers from other dependencies

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Tipoff\Reviews\Tests;
 
+use DrewRoberts\Media\MediaServiceProvider;
 use Laravel\Nova\NovaCoreServiceProvider;
 use Spatie\Permission\PermissionServiceProvider;
 use Tipoff\Addresses\AddressesServiceProvider;
 use Tipoff\Authorization\AuthorizationServiceProvider;
 use Tipoff\Locations\LocationsServiceProvider;
+use Tipoff\Seo\SeoServiceProvider;
 use Tipoff\Reviews\ReviewsServiceProvider;
 use Tipoff\Support\SupportServiceProvider;
 use Tipoff\TestSupport\BaseTestCase;
@@ -22,11 +24,13 @@ class TestCase extends BaseTestCase
             NovaCoreServiceProvider::class,
             NovaPackageServiceProvider::class,
             SupportServiceProvider::class,
-            AddressesServiceProvider::class,
-            AuthorizationServiceProvider::class,
             PermissionServiceProvider::class,
-            ReviewsServiceProvider::class,
+            AuthorizationServiceProvider::class,
+            AddressesServiceProvider::class,
+            MediaServiceProvider::class,
+            SeoServiceProvider::class,
             LocationsServiceProvider::class,
+            ReviewsServiceProvider::class,
         ];
     }
 }
